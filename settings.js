71452e461eba942ba1267851ca79081222f4cef8
Oliver0812 <121099176+Oliver0812@users.noreply.github.com>
2025-04-06 01:43:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'ADCENTRAL CUSTOMER SERVICE', // Your name
-  ownerNumber: '+2349029868042', //Your number
+  botOwner: 'GET YOUR BOT FOR 15K', // Your name
+  ownerNumber: '+2349135732182', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
